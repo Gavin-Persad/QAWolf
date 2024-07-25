8013ffe35f5046b69efb1f43752fe0c4f1093d60
Gavin-Persad <gavinapersad@gmail.com>
2024-07-25 17:16:17 +0100

    basic plan

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
-const { chromium } = require("playwright");
+const { chromium } = require('playwright');
 
 async function sortHackerNewsArticles() {
-  // launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+	// launch browser
+	const browser = await chromium.launch({ headless: false });
+	const context = await browser.newContext();
+	const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+	// go to Hacker News
+	await page.goto('https://news.ycombinator.com/newest');
 }
 
 (async () => {
-  await sortHackerNewsArticles();
+	await sortHackerNewsArticles();
+
+	// close browser
+	process.exit(0);
 })();
---
+++ plan.md
@@ -0,0 +1,6 @@
+//navigate to page in playwright
+//take screenshot - full page
+//create path to find 100 newest articles
+// return 100 newest articles
+//stretch
+//find what data is displayed and find best way of breaking this down
